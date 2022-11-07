e538a9f82e52c587a63aaa095cfd8881d730f4fb
natesway <64743334+natesway@users.noreply.github.com>
2022-11-07 15:14:21 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.17.tar.xz
 sudo apt install libc6-dev ncurses-dev gcc make binutils elfutils flex bison devscripts libssl-dev python-pytest
 tar -Jxvf linux-5.19.17.tar.xz
-cd linux*
+cd linux-5.19.17
 wget https://raw.githubusercontent.com/eupnea-linux/kernel/main/kernel.conf
 mv kernel.conf .config
 make oldconfig
