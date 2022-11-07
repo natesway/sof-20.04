930fff1a9e045d2b97362f0de8250cec67106ac1
natesway <64743334+natesway@users.noreply.github.com>
2022-11-07 15:07:51 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.17.tar.xz
+sudo apt install libc6-dev ncurses-dev gcc make binutils elfutils flex bison devscripts libssl-dev python-pytest
+tar -Jxvf linux-5.19.17.tar.xz
+cd linux*
+wget https://raw.githubusercontent.com/eupnea-linux/kernel/main/kernel.conf
+mv kernel.conf .config
+make oldconfig
+make testconfig
+make -j2 bindeb-pkg
+mkdir ../deb
+mv *.deb ../deb
